d1e703d8e6890b06a02de3301fd6f34e19c1bef5
Innovanito <khg05311@naver.com>
2023-06-28 19:57:46 +0900

    0628

---
+++ src/Home.tsx
@@ -0,0 +1,12 @@
+import Button from 'react-bootstrap/Button';
+
+const Home = () => {
+  return (
+    <>
+      <div>Home</div>
+      <Button variant="primary">Click me! twice2</Button>
+    </>
+  )
+}
+
+export default Home
