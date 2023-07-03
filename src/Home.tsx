588e52a541a235d18ce69873ea17578591b2b7a6
Innovanito <khg05311@naver.com>
2023-07-03 17:47:47 +0900

    feed making

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,14 +3,25 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { FaCompass, FaHome, FaPlus } from 'react-icons/fa';
+
+import {  FaPlus } from 'react-icons/fa';
+import { MdLocalDrink } from "react-icons/md";
+import { GiHotMeal, GiCupcake } from "react-icons/gi";
+
 import { NavLink } from 'react-router-dom';
+import Feed from './Feed';
+import { useState } from 'react';
 
 function Home() {
+  const [leftNavData, setLeftNavData] = useState<string>('meal')
+
   return ( 
   <>
     <UpperNavbar/>
-    <LeftSidebar/>
+      <div className='flex '>
+        <LeftSidebar/>
+        <Feed leftNavData={leftNavData} />
+      </div>
   </>
   );
 }
@@ -21,7 +32,7 @@ function UpperNavbar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary" variant='success'>
       <Container fluid>
-        <Navbar.Brand href="#" className='px-5'>Foodie</Navbar.Brand>
+        <Navbar.Brand href="#" className='px-5 text-4xl font-semibold'>Foodie</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           
@@ -42,11 +53,11 @@ function UpperNavbar() {
           >  
           </Nav>
         </Navbar.Collapse>
-        <Button variant="secondary" className="d-flex align-items-center">
+        <Button variant="outline-secondary" className="d-flex align-items-center">
           <FaPlus size={15} className="me-1" />
           <span style={{ fontSize: "12px" }}>업로드</span>
         </Button>
-        <Button variant="success" className='mx-2'>Login</Button>{' '}
+        <Button variant="outline-success" className='mx-2'>Login</Button>{' '}
       </Container>
     </Navbar>
   )
@@ -67,11 +78,11 @@ const LeftSidebar = () => {
               };
             }}
           >
-            <FaHome />
-            <h1 className="px-4">Your pick</h1>
+            <GiHotMeal/>
+            <h1 className="px-4">Meal</h1>
           </NavLink>
           <NavLink
-            to={'/explore'}
+            to={'/dessert'}
             className="d-flex px-2 text-3xl align-items-center"
             style={({ isActive }) => {
               return {
@@ -80,8 +91,21 @@ const LeftSidebar = () => {
               };
             }}
           >
-            <FaCompass />
-            <h1 className="px-4">Find more</h1>
+            <GiCupcake />
+            <h1 className="px-4">Dessert</h1>
+          </NavLink>
+          <NavLink
+            to={'/beverage'}
+            className="d-flex px-2 text-3xl align-items-center py-3"
+            style={({ isActive }) => {
+              return {
+                fontWeight: isActive ? "bold" : "",
+                color: isActive ? "red" : "black",
+              };
+            }}
+          >
+            < MdLocalDrink/>
+            <h1 className="px-4">Drinks</h1>
           </NavLink>
         </div>
       </Container>
